755174d0240a5c15b30fa48437f4a591679bc056
genka <tamani.coding@gmail.com>
2022-12-24 11:43:35 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// SCENE
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xa8def0);
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = 5;
+camera.position.z = 10;
+camera.position.x = -13;
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true
+
+// ORBIT CAMERA CONTROLS
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true
+orbitControls.enablePan = true
+orbitControls.minDistance = 5
+orbitControls.maxDistance = 20
+// orbitControls.maxPolarAngle = Math.PI / 2 - 0.05 // prevent camera below ground
+// orbitControls.minPolarAngle = Math.PI / 4        // prevent top down view
+orbitControls.update();
+
+const dLight = new THREE.DirectionalLight('white', 0.6);
+dLight.position.x = 20;
+dLight.position.y = 30;
+dLight.castShadow = true;
+dLight.shadow.mapSize.width = 4096;
+dLight.shadow.mapSize.height = 4096;
+const d = 35;
+dLight.shadow.camera.left = - d;
+dLight.shadow.camera.right = d;
+dLight.shadow.camera.top = d;
+dLight.shadow.camera.bottom = - d;
+scene.add(dLight);
+
+const aLight = new THREE.AmbientLight('white', 0.4);
+scene.add(aLight);
+
+// ANIMATE
+document.body.appendChild(renderer.domElement);
+
+// RESIZE HANDLER
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize);
+
+let gameLoop = () => {
+    orbitControls.update()
+    renderer.render(scene, camera);
+    setTimeout(gameLoop, 16);
+};
+gameLoop();
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'index.js',
+    path: dist
+  },
+  devServer: {
+    static: dist,
+  },
+}
